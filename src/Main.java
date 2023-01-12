21e7376c198c1b928f175ee9b52a3b321058f1cd
Lucas <122219985+luukEes@users.noreply.github.com>
2023-01-12 08:54:36 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         return StringUtils.reverse(str);
     }
     public static String stringFromArray(String[] str) {
-        return String
+        return Arrays.toString(strArray);
     }
     public static void main(String[] args) {
         /* System.out.println(reverseString("Tak")); */
