6171709f44eaae4a8d0521b26915b1e557889243
Lucas Sowul <sowul.lukas@gmail.com>
2023-01-12 18:40:21 +0100

    repaired

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 import org.apache.commons.lang3.StringUtils;
+import java.util.Arrays;
+
 public class Main {
-    public static String reverseString(String str) {
-        return StringUtils.reverse(str);
-    }
     public static String stringFromArray(String[] str) {
-         String joinedString = Arrays.toString(strArray);
-         StringUtils.deleteWhitespaces(joinedString);
+         String joinedString = Arrays.toString(str);
         return joinedString;
     }
     public static void main(String[] args) {
-        /* System.out.println(reverseString("Tak")); */
-    }   System.out.println(stringFromArray(String[] str {"Iam", "curious" , "is", "it", "gonna", "work"));
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(stringFromArray(cars));
+    }
 }
+
